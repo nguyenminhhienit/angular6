11917ec6145712a36a48e43391e87729e5bd05cb
hienminh <nguyenminhhienit@gmail.com>
2018-08-10 15:47:54 +0700

    09-8-2018

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -8,22 +7,25 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {  
+  
   selectedHero: Hero;
   //heroes = HEROES;
   heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }  
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
-  getHeroes(): void{
-    this.heroes = this.heroService.getHeroes();
+  getHeroes(): void{    
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
-
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit() {
-    this.getHeroes();
-  }  
 }
